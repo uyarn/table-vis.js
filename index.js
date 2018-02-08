86584c7bea1a2d4d61ca38f45350790897967bec
yarnchen <yarnchenyuyang@gmail.com>
2018-02-08 18:56:03 +0000

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ *  Tabular-vis.js
+ */
+
+'use strict';
+
+const tabular_vis = require('/src/tabular_vis.js');
+
+
+
+//exports the module
+if(typeof module!== 'undefined' && module.exports){
+  module.exports = tabular_vis;
+}
---
+++ src/tabular_vis.js
@@ -0,0 +1,16 @@
+
+
+var tabular_vis = function(option){
+  'use strict'
+
+  function add(option){
+    return option+1;
+
+  }
+
+}
+
+
+if(typeof module!== 'undefined' && module.exports){
+  module.exports = tabular_vis;
+}
