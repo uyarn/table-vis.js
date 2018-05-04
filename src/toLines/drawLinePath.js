7e41dbc0b299533f4ac728eca10f2c74fcea8796
yarnchen <yarnchenyuyang@gmail.com>
2018-05-04 19:45:37 +0100

    line chart style modification

--- src/toLines/drawLinePath.js
+++ src/toLines/drawLinePath.js
@@ -6,22 +6,21 @@ const drawLinePath=(ctx,data,width,height,scale,lineStyle,focI)=>{
     if(i==focI){
           ctx.arc((width/data.length)*i*0.92+10,height-scale(d),10,0,2*Math.PI);
           ctx.fillStyle= lineStyle[0];
-          ctx.strokeStyle = lineStyle[0];
-          ctx.stroke();
         }
       else{
           ctx.arc((width/data.length)*i*0.92+10,height-scale(d),5,0,2*Math.PI);
           ctx.fillStyle= lineStyle[1];
-          ctx.strokeStyle = lineStyle[1];
-          ctx.stroke();
         }
+    ctx.closePath();
     ctx.fill();
+    ctx.beginPath();
     if(i!=data.length-1){
       ctx.moveTo((width/data.length)*i*0.92+10,height-scale(d));
       ctx.lineTo((width/data.length)*(i+1)*0.92+10,height-scale(data[i+1]));
-      //ctx.strokeStyle= lineStyle[1];
-      ctx.stroke();
+      ctx.strokeStyle= lineStyle[1];    
     }
+    ctx.closePath();
+    ctx.stroke();
   });
 
 
