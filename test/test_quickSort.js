1eb1412cd6a01f33e6821001115af5406c446376
yarnchen <yarnchenyuyang@gmail.com>
2018-07-12 22:59:11 +0100

    use mocha test framework and make it compatible with es2015

---
+++ test/test_quickSort.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+import quickSort from '../src/Module_BoxPlot/quickSort'
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
