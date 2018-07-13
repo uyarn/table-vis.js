f1c5a41b6e82f9a77c999df16e8efd55e52dce27
yarnchen <yarnchenyuyang@gmail.com>
2018-07-13 14:38:54 +0100

    demo page

--- test/test_quickSort.js
+++ test/test_quickSort.js
@@ -4,7 +4,7 @@ import quickSort from '../src/Module_BoxPlot/quickSort'
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+      assert.equal(quickSort([1,12,3]), [1,3,12]);
     });
   });
 });
