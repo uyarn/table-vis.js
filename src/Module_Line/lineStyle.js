ac28028bf8092095c75b9fac70d8c3f17d30c638
yarnchen <yarnchenyuyang@gmail.com>
2018-07-15 02:30:42 +0100

    coverage files remove

--- src/Module_Line/lineStyle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const lineStyle=()=>{
-  let focusPoint = 'rgba(40,125,250,1)';
-  let normalPoint = 'rgba(135,135,135,1)';
-  return [focusPoint,normalPoint];
-}
-
-export default lineStyle;
